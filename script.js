0d1778fc07fedb79e2020478f37b5cdf0190cb03
meryrowery <m.manikalo@gmail.com>
2024-09-30 16:03:19 +0200

    done

--- script.js
+++ script.js
@@ -9,22 +9,67 @@ function getRandomColor() {
 
 const container = document.querySelector("#container");
 
-for (i=0;i<256;i++) {
-    const content = document.createElement("div");
-    // content.textContent = "box";
-    content.classList.add("box")
-    container.appendChild(content);
-}
+// for (i=0;i<256;i++) {
+//     const content = document.createElement("div");
+//     // content.textContent = "box";
+//     content.classList.add("box")
+//     container.appendChild(content);
+// }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => 
+// const boxes = document.querySelectorAll(".box");
+// boxes.forEach((box) => 
+//     {box.addEventListener("mouseover", () => {
+//         box.style.backgroundColor = getRandomColor();
+//     });
+// });
+
+playGame(16)
+
+function askMe() {
+    while (true) {
+      const choice = prompt("Please provide a number for the grid");
+      if (choice <= 100) {
+        return parseInt(choice);
+      } else {
+        alert("Invalid choice. Please pick a smaller number");
+      }
+    }
+  };
+
+function playGame(numberRows) {
+    const container = document.querySelector("#container");
+    // Clear the previous grid
+    container.innerHTML = '';
+
+
+    for (row=0;row<(numberRows);row++) {
+        const divRow = document.createElement("div")
+        divRow.setAttribute("class","row")
+        for (column=0;column<numberRows;column++) {
+            const divCol = document.createElement("div");
+            divCol.setAttribute("class","column")
+            // content.textContent = "box";
+            divCol.classList.add("box")
+            divRow.appendChild(divCol);
+        }
+    container.appendChild(divRow)
+    };
+
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => 
     {box.addEventListener("mouseover", () => {
         box.style.backgroundColor = getRandomColor();
     });
 });
+}
 
 const body = document.querySelector("body")
 const button = document.createElement('button')
-button.textContent = "Button"
+button.textContent = "Start a new game"
 body.append(button)
+button.addEventListener("click", () => {
+    const gridSize = askMe();
+    playGame(gridSize)
+});
+
 
